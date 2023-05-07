4e8dd09de52fdb4536373009ce7528ec020dc72d
timdoolan <tim.doolan88@icloud.com>
2023-05-07 22:45:50 +0100

    feat: pass dynamodb endpoint as env var to enable local testing

--- clients/dynamodb.go
+++ clients/dynamodb.go
@@ -1,8 +1,10 @@
 package clients
 
 import (
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"os"
 )
 
 func DynamoDbClient() *dynamodb.DynamoDB {
@@ -10,7 +12,7 @@ func DynamoDbClient() *dynamodb.DynamoDB {
 		SharedConfigState: session.SharedConfigEnable,
 	}))
 
-	client := dynamodb.New(sess)
+	client := dynamodb.New(sess, &aws.Config{Endpoint: aws.String(os.Getenv("DYNAMODB_ENDPOINT"))})
 
 	if client == nil {
 		panic("Unable to create DynamoDB client")
