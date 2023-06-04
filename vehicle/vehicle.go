cb028da837787500060ee379b094ce9cfc1ec9dc
timdoolan <tim.doolan88@icloud.com>
2023-06-04 20:11:02 +0100

    chore: extract vehicle interface into vehicles package

---
+++ vehicle/vehicle.go
@@ -0,0 +1,16 @@
+package vehicle
+
+type Capacity struct {
+	Value int    `json:"value" binding:"required"`
+	Unit  string `json:"unit" binding:"required"`
+}
+
+type Vehicle struct {
+	Vin          string   `json:"vin" binding:"required"`
+	Manufacturer string   `json:"manufacturer" binding:"required"`
+	Model        string   `json:"model" binding:"required"`
+	Year         int      `json:"year" binding:"required"`
+	Color        string   `json:"color" binding:"required"`
+	Capacity     Capacity `json:"capacity" binding:"required"`
+	LicensePlate string   `json:"license_plate"`
+}
