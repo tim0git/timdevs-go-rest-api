271edb216da38c1d631e4e4fb77583e25fe2ac43
timdoolan <tim.doolan88@icloud.com>
2023-06-05 21:13:10 +0100

    refactor: reduce put vehicle modal cognitive complexity

--- modal/put_vehicle.go
+++ modal/put_vehicle.go
@@ -3,28 +3,42 @@ package modal
 import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
-	"os"
 	"timdevs.rest.api.com/m/v2/database"
+	"timdevs.rest.api.com/m/v2/utils"
 )
 
 func PutVehicle(item map[string]*dynamodb.AttributeValue) (*dynamodb.PutItemOutput, error) {
 	client := database.DynamoDB()
 
-	putRequest := &dynamodb.PutItemInput{
-		TableName: aws.String(os.Getenv("TABLE_NAME")),
-		Item:      item,
-	}
-
-	putRequest.ConditionExpression = aws.String(`attribute_not_exists(vin) OR (#v <> :val AND attribute_exists(vin))`)
-
-	putRequest.ExpressionAttributeNames = map[string]*string{
-		"#v": aws.String("vin"),
-	}
-	putRequest.ExpressionAttributeValues = map[string]*dynamodb.AttributeValue{
-		":val": {
-			S: aws.String(*item["vin"].S),
-		},
-	}
+	putRequest := buildPutItemInput(item)
 
 	return client.PutItem(putRequest)
+}
+
+func buildPutItemInput(item map[string]*dynamodb.AttributeValue) *dynamodb.PutItemInput {
+	return &dynamodb.PutItemInput{
+		TableName:                 aws.String(utils.GetTableName()),
+		Item:                      item,
+		ConditionExpression:       getConditionExpression(),
+		ExpressionAttributeNames:  getPutExpressionAttributeNames(),
+		ExpressionAttributeValues: getPutExpressionAttributeValues(item),
+	}
+}
+
+func getConditionExpression() *string {
+	return aws.String(`attribute_not_exists(vin) OR (#v <> :val AND attribute_exists(vin))`)
+}
+
+func getPutExpressionAttributeNames() map[string]*string {
+	return map[string]*string{
+		"#v": aws.String("vin"),
+	}
+}
+
+func getPutExpressionAttributeValues(item map[string]*dynamodb.AttributeValue) map[string]*dynamodb.AttributeValue {
+	return map[string]*dynamodb.AttributeValue{
+		":val": {
+			S: item["vin"].S,
+		},
+	}
 }
