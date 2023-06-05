916ec3d9aaa4bb0a83861b33540e736e4f76cfa3
timdoolan <tim.doolan88@icloud.com>
2023-06-05 21:13:17 +0100

    refactor: reduce update vehicle modal cognitive complexity

--- modal/update_vehicle.go
+++ modal/update_vehicle.go
@@ -2,11 +2,10 @@ package modal
 
 import (
 	"fmt"
-	"os"
-
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"timdevs.rest.api.com/m/v2/database"
+	"timdevs.rest.api.com/m/v2/utils"
 	"timdevs.rest.api.com/m/v2/vehicle"
 )
 
@@ -14,7 +13,7 @@ func UpdateVehicle(vehicle vehicle.Update, vin string) (*dynamodb.UpdateItemOutp
 	client := database.DynamoDB()
 
 	updateRequest := &dynamodb.UpdateItemInput{
-		TableName: aws.String(os.Getenv("TABLE_NAME")),
+		TableName: aws.String(utils.GetTableName()),
 		Key: map[string]*dynamodb.AttributeValue{
 			"vin": {
 				S: aws.String(vin),
@@ -22,8 +21,8 @@ func UpdateVehicle(vehicle vehicle.Update, vin string) (*dynamodb.UpdateItemOutp
 		},
 		ConditionExpression:       aws.String("attribute_exists(vin)"),
 		UpdateExpression:          aws.String(getUpdateExpression()),
-		ExpressionAttributeValues: getExpressionAttributeValues(vehicle),
-		ExpressionAttributeNames:  getExpressionAttributeNames(),
+		ExpressionAttributeValues: getUpdateExpressionAttributeValues(vehicle),
+		ExpressionAttributeNames:  getUpdateExpressionAttributeNames(),
 	}
 
 	return client.UpdateItem(updateRequest)
@@ -33,7 +32,7 @@ func getUpdateExpression() string {
 	return "set #manufacturer = :manufacturer, #model = :model, #year = :year, #color = :color, #batteryCapacity = :batteryCapacity"
 }
 
-func getExpressionAttributeValues(vehicle vehicle.Update) map[string]*dynamodb.AttributeValue {
+func getUpdateExpressionAttributeValues(vehicle vehicle.Update) map[string]*dynamodb.AttributeValue {
 	expressionAttributeValues := map[string]*dynamodb.AttributeValue{
 		":manufacturer": {
 			S: aws.String(vehicle.Manufacturer),
@@ -61,7 +60,7 @@ func getExpressionAttributeValues(vehicle vehicle.Update) map[string]*dynamodb.A
 	return expressionAttributeValues
 }
 
-func getExpressionAttributeNames() map[string]*string {
+func getUpdateExpressionAttributeNames() map[string]*string {
 	expressionAttributeNames := map[string]*string{
 		"#model":           aws.String("model"),
 		"#year":            aws.String("year"),
