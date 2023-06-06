7cb31b17c6b837114c39077f7f4518e7641b908c
timdoolan <tim.doolan88@icloud.com>
2023-06-06 10:01:44 +0100

    feat: add charging status field to vehicle struct

--- vehicle/vehicle.go
+++ vehicle/vehicle.go
@@ -5,14 +5,21 @@ type Capacity struct {
 	Unit  string `json:"unit" binding:"required"`
 }
 
+type ChargingStatus struct {
+	Charging     bool   `json:"charging"`
+	LocationCode string `json:"location_code"`
+	UnitId       string `json:"unit_id"`
+}
+
 type Vehicle struct {
-	Vin          string   `json:"vin" binding:"required"`
-	Manufacturer string   `json:"manufacturer" binding:"required"`
-	Model        string   `json:"model" binding:"required"`
-	Year         int      `json:"year" binding:"required"`
-	Color        string   `json:"color" binding:"required"`
-	Capacity     Capacity `json:"capacity" binding:"required"`
-	LicensePlate string   `json:"license_plate"`
+	Vin            string         `json:"vin" binding:"required"`
+	Manufacturer   string         `json:"manufacturer" binding:"required"`
+	Model          string         `json:"model" binding:"required"`
+	Year           int            `json:"year" binding:"required"`
+	Color          string         `json:"color" binding:"required"`
+	Capacity       Capacity       `json:"capacity" binding:"required"`
+	LicensePlate   string         `json:"license_plate"`
+	ChargingStatus ChargingStatus `json:"charging_status"`
 }
 
 type Update struct {
